3a44807794779e1e4a35721a3d92bdbf59708204
Keiji Ohara <keiji0@gmail.com>
2012-11-10 18:01:53 +0900

    new ubuntu branch

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
 cd $(dirname $0)
-ins(){ ln -Fs "$PWD/$1" "$2" && echo install $1; }
+ins(){ ln -fFs "$PWD/$1" "$2" && echo install $1; }
+has(){ which $1 > /dev/null; }
 
 ins .gitconfig ~/
 ins .Xdefaults ~/
+ins .config/user-dirs.dirs ~/.config/user-dirs.dirs
+
+if has gsettings; then
+	# nautilus
+	(cd ~ && mkdir Desktop Downloads Templates Public Documents Music Pictures Videos)
+	gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+	# package
+	sudo apt-get install p7zip-full
+	sudo apt-get install vlc
+fi
