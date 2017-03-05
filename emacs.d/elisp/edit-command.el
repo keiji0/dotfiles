dc3b2de003fb54c3b04927c7666d4216c2da87e7
keiji0 <keiji0@gmail.com>
2017-03-05 15:42:32 +0900

    非正規表現で検索できるoccurコマンドを追加した

--- emacs.d/elisp/edit-command.el
+++ emacs.d/elisp/edit-command.el
@@ -13,4 +13,9 @@
   (let ((reg (bounds-of-thing-at-point 'sexp)))
     (downcase-region (car reg) (cdr reg))))
 
+(defun non-regexp-occur (regexp &optional nlines)
+  "非正規表現版のoccur"
+  (interactive (occur-read-primary-args))
+  (occur-1 (regexp-quote regexp) nlines (list (current-buffer))))
+
 (provide 'edit-command)
