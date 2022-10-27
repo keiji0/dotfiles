7058081d0c234a155f34aaa44dace915c9a62ee2
keiji0 <keiji0@gmail.com>
2022-10-27 18:36:38 +0900

    VenturaからmacOS全体でアクセントメニューが表示されてキーリピートが効かないのでOffにする

--- config/vscode/setup.sh
+++ config/vscode/setup.sh
@@ -5,6 +5,3 @@ cd $(dirname $0)
 
 # Visual Studio Codeをインストール
 brew install visual-studio-code --cask
-
-# VSCodeとvimプラグインで長押しが効かない対応
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
