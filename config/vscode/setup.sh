53a7e58c08cf92fc08a3a18bbe2506228a37f8d5
keiji0 <keiji0@gmail.com>
2024-04-25 23:36:31 +0900

    vscodeのキーバインドを追加

--- config/vscode/setup.sh
+++ config/vscode/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 set -eu
+. "$DOTDIR/lib/install.sh"
 cd "$(dirname "$0")"
 
 # Visual Studio Codeをインストール
 brew install visual-studio-code --cask
+# キーバインドを設定
+lnk keybindings.json "$HOME/Library/Application\ Support/Code/User/keybindings.json"
