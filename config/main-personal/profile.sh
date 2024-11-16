f031f50e6fcd77277600e5ac9502aff18884726f
keiji0 <keiji0@gmail.com>
2024-11-16 13:49:35 +0900

    vmwareはもう使わないので設定から外す

--- config/main-personal/profile.sh
+++ config/main-personal/profile.sh
@@ -2,9 +2,3 @@
 
 # Homebrewの環境設定
 eval "$($HOMEBREW shellenv)"
-
-# vmwareの設定
-export VMWARE_ROOT=/Applications/VMware\ Fusion.app
-if [ -d "$VMWARE_ROOT" ]; then
-    _add_to_path "$VMWARE_ROOT/Contents/Library"
-fi
